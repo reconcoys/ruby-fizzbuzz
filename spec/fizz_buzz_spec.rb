4a433b58a73acc04c472555df1254cfc03daae21
Connor <csweeney@pillartechnology.com>
2016-02-17 21:59:14 -0500

    Removed tests that are covered by later tests

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -14,19 +14,16 @@ describe FizzBuzz do
     expect(@fb.fizzbuzz(2)).to eq(2)
   end
 
-  it 'should return fizz when 3 is passed in' do
-    expect(@fb.fizzbuzz(3)).to eq('fizz')
-  end
   it 'should return fizz when multiple of 3' do
     expect(@fb.fizzbuzz(9)).to eq('fizz')
   end
-  it 'should return buzz when 5 is passed in' do
-    expect(@fb.fizzbuzz(5)).to eq('buzz')
-  end
+
   it 'should return buzz when multiple of 5 is passed in' do
     expect(@fb.fizzbuzz(10)).to eq('buzz')
   end
+
   it 'should return fizzbuzz when multiple of both 5 and 3 is passed in' do
     expect(@fb.fizzbuzz(15)).to eq('fizzbuzz')
   end
+
 end
